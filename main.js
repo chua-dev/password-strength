3916cab7ab588c7a04400803aec4c88dd02f4673
chua <ongwei188@gmail.com>
2022-02-05 20:23:05 +0800

    complete

---
+++ main.js
@@ -0,0 +1,54 @@
+const input = document.querySelector("input"),
+      showHide = document.querySelector(".show-hide"),
+      indicator = document.querySelector(".indicator"),
+      iconText = document.querySelector(".icon-text"),
+      text = document.querySelector(".text");
+
+// Show and hide password
+showHide.addEventListener("click", () => {
+    if(input.type === "password"){
+        input.type = "text"
+        showHide.classList.replace("fa-eye-slash", "fa-eye");
+    } else {
+        input.type = "password"
+        showHide.classList.replace("fa-eye", "fa-eye-slash")
+    }
+})
+
+// Show Password strength using Regex
+let alphabet = /[a-zA-Z]/,
+    numbers = /[0-9]/,
+    scharacters = /[!,@,#,$,%,^,&,*,?,_,(,),-,+,=,~]/
+
+input.addEventListener("keyup", () => {
+    indicator.classList.add("active");
+
+    let val = input.value;
+    if(val.match(alphabet) || val.match(numbers) || val.match(scharacters)) {
+        text.textContent = "Password is weak";
+        iconText.style.color = "#FF6333"
+        input.style.borderColor = "#FF6333"
+        showHide.style.color = "#FF6333"
+    }
+
+    if(val.match(alphabet) && val.match(numbers) && val.length >= 6){
+        text.textContent = "Password is medium";
+        iconText.style.color = "#cc8500"
+        input.style.borderColor = "#cc8500"
+        showHide.style.color = "#cc8500"
+    }
+
+    if(val.match(alphabet) && val.match(numbers) && val.match(scharacters) && val.length >= 6){
+        text.textContent = "Password is strong";
+        iconText.style.color = "#22C32A"
+        input.style.borderColor = "#22C32A"
+        showHide.style.color = "#22C32A"
+    }
+
+    if(val == ""){
+        indicator.classList.remove("active");
+        iconText.style.color = "#A6A6A6"
+        input.style.borderColor = "#A6A6A6"
+        showHide.style.color = "#A6A6A6"
+    }
+});
